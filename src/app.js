978ea062de26d7b776e9a7cc67eea29764800ba2
Kalvium <109285740+Kalvium-Program@users.noreply.github.com>
2022-11-14 13:54:39 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,53 @@
+//Progression 1 - create a Manager array and return it
+let managerName = "Alex Ferguson";
+let managerAge = 78;
+let currentTeam = "Manchester FC";
+let trophiesWon = 27;
+
+//Write your function here
+
+// Don't edit the following code
+try {
+  var manager = createManager(
+    managerName,
+    managerAge,
+    currentTeam,
+    trophiesWon
+  );
+} catch (e) {
+  // do nothing - expected error
+}
+
+//Progression 2 - create a formation object and return it
+var formation = [4, 4, 3];
+
+//write your function here
+
+// Dont edit the following code
+
+try {
+  var formationObject = createFormation(formation);
+} catch (e) {
+  //do nothing
+}
+
+//Progression 3 - Filter players that debuted in ___ year
+
+//Progression 4 - Filter players that play at the position _______
+
+//Progression 5 - Filter players that have won ______ award
+
+//Progression 6 - Filter players that won ______ award ____ times
+
+//Progression 7 - Filter players that won ______ award and belong to ______ country
+
+//Progression 8 - Filter players that won atleast ______ awards, belong to ______ team and are younger than ____
+
+//Progression 9 - Sort players in descending order of their age
+
+//Progression 10 - Sort players beloging to _____ team in descending order of awards won
+
+//Challenge 1 - Sort players that have won _______ award _____ times and belong to _______ country in alphabetical order of their names
+
+//Challenge 2 - Sort players that are older than _____ years in alphabetical order
+//Sort the awards won by them in reverse chronological order
